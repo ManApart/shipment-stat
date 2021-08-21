b3e7827ea2084cd806ad34d172eba00ef0d83e61
ManApart <r@austinkucera.com>
2021-08-21 11:12:34 -0400

    misc general stats

--- src/main/kotlin/Stats.kt
+++ src/main/kotlin/Stats.kt
@@ -2,7 +2,8 @@ import parsing.ShipmentDay
 
 data class Stats(val header: Header, val days: List<ShipmentDay>){
     val calendar = ShipmentCalendar(days)
-    val totalCount: Long by lazy { calendar.getPeriod(calendar.timePeriod).totalCount }
-    val totalRevenue: Long by lazy { calendar.getPeriod(calendar.timePeriod).totalRevenue }
+    val totals = calendar.getPeriod(calendar.timePeriod)
+    val totalCount = totals.totalCount
+    val totalRevenue = totals.totalRevenue
     val daysShipped = days.filter { it.entries.isNotEmpty() }.size
 }
--- src/main/kotlin/parsing/ShipmentStat.kt
+++ src/main/kotlin/parsing/ShipmentStat.kt
@@ -8,7 +8,17 @@ fun main() {
     val config = readConfig()
     val stats = parseRawStats(File(config["statPath"]!! as String).readLines())
 
-    println("During the period ${stats.calendar.timePeriod} " + stats.header.farmName + " has shipped " + stats.totalCount.format() + " items through " +stats.daysShipped + " shipments from ${stats.header.farmName} Farm, earning $" + stats.totalRevenue.format() + ".")
+    val farmer = stats.header.playerName
+
+    println("During the period ${stats.calendar.timePeriod} " + farmer + " has shipped " + stats.totalCount.format() + " items through " + stats.daysShipped + " shipments from ${stats.header.farmName} Farm, earning $" + stats.totalRevenue.format() + ".")
+
+    println("$farmer has sold " + stats.totals.products.size + " different products in " + stats.totals.products.values.groupBy { it.category }.size + " categories.")
+
+    val mostProfitable = stats.totals.products.values.maxByOrNull { it.totalRevenue }!!
+    println("$farmer's most profitable crop is ${mostProfitable.id}. ${mostProfitable.totalCount.format()} sold for a profit of ${mostProfitable.totalRevenue.format()}")
+
+    val mostSold = stats.totals.products.values.maxByOrNull { it.totalCount }!!
+    println("$farmer's most sold crop is ${mostSold.id}. ${mostSold.totalCount.format()} sold for a profit of ${mostSold.totalRevenue.format()}")
 
 }
 
