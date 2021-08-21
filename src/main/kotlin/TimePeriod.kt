a800b645c7416de80de92d076039c7dc1f48907d
ManApart <r@austinkucera.com>
2021-08-21 10:46:00 -0400

    time period tests

--- src/main/kotlin/TimePeriod.kt
+++ src/main/kotlin/TimePeriod.kt
@@ -1,5 +1,5 @@
 data class TimePeriod(val start: StardewDate, val end: StardewDate) {
     fun includes(date: StardewDate): Boolean {
-        return false
+        return date in start..end
     }
 }
--- src/test/kotlin/TimePeriodTest.kt
+++ src/test/kotlin/TimePeriodTest.kt
@@ -1,11 +1,19 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class TimePeriodTest {
 
     @Test
     fun includes() {
-        assertEquals(true, true)
+        val period = TimePeriod(StardewDate(0, 0, 1), StardewDate(1, 2, 3))
+        assertTrue(period.includes(StardewDate(0, 0, 1)))
+        assertTrue(period.includes(StardewDate(0, 1, 0)))
+        assertTrue(period.includes(StardewDate(1, 2, 3)))
+
+        assertFalse(period.includes(StardewDate(0, 0, 0)))
+        assertFalse(period.includes(StardewDate(1, 2, 4)))
+        assertFalse(period.includes(StardewDate(2, 0, 0)))
     }
 
 }
